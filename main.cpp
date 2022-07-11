4d18a51755d6b04f39f7b5d88b4e2f4c74243c99
Sovan Patnayak <sovanpatnayak@gmail.com>
2022-07-11 18:16:20 -0400

    Skeleton for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <curses.h>
+#include <string>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+
+void curses_init() {
+     initscr();                 // Start curses mode
+     noecho();                  // Don't repeat keypresses
+     cbreak();                  // Disable line buffering
+     keypad(stdscr, true);      // Enable special keys to be recorded
+}
+
+
+int main(int argc, char *argv[]) {
+    std::string file_name = ""; // Create string variable
+
+    if (argc > 1) {
+        file_name = argv[1];    // Set the file name
+    }
+
+    curses_init();              // Initialize curses
+
+    refresh();                  // Refresh curses
+    endwin();                   // End curses mode
+
+    return 0;
 }
