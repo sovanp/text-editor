8d00ab791d4f9efecb91774f6083111da279d8f5
Sovan Patnayak <sovanpatnayak@gmail.com>
2022-07-11 20:11:42 -0400

    Implemented Buffer class

---
+++ buffer.cpp
@@ -0,0 +1,27 @@
+#include "buffer.h"
+
+Buffer::Buffer() {}
+
+std::string Buffer::sub_tabs(std::string line) {
+    int tab = line.find("\t");
+    if (tab == line.npos) {
+        return line;
+    }
+    else {
+        return sub_tabs(line.replace(tab, 1, "    "));
+    }
+}
+
+void Buffer::insert_line(std::string line, int n) {
+    line = sub_tabs(line);
+    lines.insert(lines.begin()+n, line);
+}
+
+void Buffer::append_line(std::string line) {
+    line = sub_tabs(line);
+    lines.push_back(line);
+}
+
+void Buffer::remove_line(int n) {
+    lines.erase(lines.begin()+n);
+}
